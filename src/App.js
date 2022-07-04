e6142b6122d9e54bcdf3deafed2e9335ecdbbff2
Ferrin <fthreatt19@gmail.com>
2022-07-04 19:00:11 -0400

    formatting css of flyer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import {useEffect, useState} from "react";
 import './App.css';
-import React from 'react'
+import React from 'react';
 import background from "./images/background.jpeg";
 
 function App() {
-    const CLIENT_ID = ""
+    const CLIENT_ID = "191d6ec4327045139479d0bec839da14"
     // const REDIRECT_URI = "https://coachella-lineup.web.app/"
-    const REDIRECT_URI = "http://localhost:5005/"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
+    const REDIRECT_URI = "http://localhost:3000/"
+    // const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    // const RESPONSE_TYPE = "token"
 
     const [token, setToken] = useState("")
     
@@ -50,7 +50,7 @@ function App() {
         window.localStorage.removeItem("token")
     }
 
-   
+    console.log("hi");    
     const topTracks = async (e) => { 
       const response = await fetch('https://api.spotify.com/v1/me/top/artists/?limit=50',{
         method: 'GET', headers: {
@@ -59,28 +59,47 @@ function App() {
           'Authorization': 'Bearer ' + token
         }
       });
-
+      console.log("hi");    
       const responseJson = await response.json();
       console.log(responseJson);
-      // for(var j = 0; j < 10; j++ ){
-      //   // const first10Artist = document.getElementById('song-title');
-      //   // first10Artist.innerText += " "+ responseJson.items[j].name + " ";
-      //   // console.log(" "+ responseJson.items[j].name + " ");
-      //   console.log("hi");
-      // }
-
-      for(var i = 0; i < responseJson.items.length; i++){   
-        const playlistTitleElement = document.getElementById('song-title');
-        playlistTitleElement.innerText += " "+ responseJson.items[i].name + " ";
-        const genreElemenet = document.getElementById('genre');
-        genreElemenet.innerText += "  " + responseJson.items[i].genres + "  ";      
-   }
-  
       
-  
+      for(var i = 0; i < 10; i++){   
+        // const playlistTitleElement = document.getElementById('song-title');
+        // playlistTitleElement.innerText += " "+ responseJson.items[i].name + " ";
+        const genreElemenet = document.getElementById('genre');
+        genreElemenet.innerText += "  " + responseJson.items[i].genres + "  ";  
+            
+   }
 
+//    for(var j = 0; j < responseJson.items.length; j++){   
+//     const playlistTitleElement2 = document.getElementById('song-container');
+//     playlistTitleElement2.innerText += " "+ responseJson.items[j].name + " ";
+//     // const genreElemenet = document.getElementById('genre');
+//     // genreElemenet.innerText += "  " + responseJson.items[i].genres + "  ";  
+        
+// }
+  
+    
+      const firstArtist = document.getElementById('first-artists');
+      firstArtist.innerText += " "+ responseJson.items[0].name + " ";
+     
+    for(var k = 2; k < 25; k++ ){
+      const first5Artist = document.getElementById('first-five-artists');
+      first5Artist.innerText += " "+ responseJson.items[k].name + " ";
+      
     }
 
+    const secondArtist = document.getElementById('second-artists');
+    secondArtist.innerText += " "+ responseJson.items[1].name + " ";
+
+    for(var h = 25; h < 50; h++ ){
+      const first5Artist = document.getElementById('next-25-artists');
+      first5Artist.innerText += " "+ responseJson.items[h].name + " ";
+     
+    }
+
+    }
+    
     return (
         <div className="App">
             <header className="App-header">
@@ -90,26 +109,29 @@ function App() {
                 <h1>Coachella LineUp</h1>
                
                 {!token ?
-                    <a href={authUrl}>Login!!
+                    <a href={authUrl}>Login
                         to Spotify</a>
                    :  <button id="submitButton" onClick={topTracks}>get started</button>
                   
                     }
 
-                {token ?
-                   	
+                {token ?	
                      <div>  
                       <button onClick={logout}>Logout</button> 
                 
                     <div id="song-container">
-                    
-                       <p className='title' id="song-title"></p>
-                       
-                      
+                    <p className='first-one' id="first-artists"></p>
+                       <p className='first-five' id="first-five-artists"></p>
+                       <br></br>
+                       <p className='second-one' id="second-artists"></p>
+                       <p className='last-few' id="next-25-artists"></p>
+                       <br></br>
+                       <p className='top-genres'>Your top genres</p>
                      </div>
-                    
+                     
                      
                      <div>
+                     
                      <p className="genres" id="genre"></p>
                      
                    </div>
@@ -118,8 +140,6 @@ function App() {
                   
                      : <div></div>
                  }
-
-              
 
             </header>
            
---
+++ src/pages/lineup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import '../App.js';
+
+function Lineup() {
+    return(
+        <div>
+            
+        </div>
+
+    );
+
+}
+
+export default Lineup;
